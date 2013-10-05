e9b47280f93543abce405fbbadbcdd79a07b8291
mac <mac@macs-MacBook-Air.local>
2013-10-05 20:39:44 +0800

    Initial Commit

---
+++ nurse180/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  nurse180
+//
+//  Created by mac on 10/5/13.
+//  Copyright (c) 2013 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ nurse180/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  nurse180
+//
+//  Created by mac on 10/5/13.
+//  Copyright (c) 2013 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
